549eafe607b2bb147444161f6c9a13155235f9a1
erikgonzaga <erikgonzaga@outlook.com>
2022-10-03 23:45:20 -0300

    Criação de pacotes de Classe de Utilitários. - Classe DataUtil, conversão de entrada de dados no Formatado String (pradrão br.) para LocalDate e DateTimeFormatter.

---
+++ src/main/java/br/com/gonzaga/mybankproject/Utils/DateUtil.java
@@ -0,0 +1,12 @@
+package br.com.gonzaga.mybankproject.Utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtil {
+
+    public static LocalDate stringToLocalDate(String date, String format) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return LocalDate.parse(date, formatter);
+    }
+}
