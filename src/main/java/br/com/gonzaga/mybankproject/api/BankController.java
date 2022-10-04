502ab6f4bb8323b2abbf3a3257a5d31fb622bf9a
erikgonzaga <erikgonzaga@outlook.com>
2022-10-04 12:58:11 -0300

    Corrigindo requisição de Dados: - Injeção de BankService em BankController - Parametro de createAccount - requisição em bankService.

--- src/main/java/br/com/gonzaga/mybankproject/api/BankController.java
+++ src/main/java/br/com/gonzaga/mybankproject/api/BankController.java
@@ -1,16 +1,30 @@
 package br.com.gonzaga.mybankproject.api;
 
+import br.com.gonzaga.mybankproject.services.BankService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import request.AccountRequest;
 
 @RestController
 public class BankController {
 
+    // Injetando o corpo da requisição em bankService
+    @Autowired
+    private BankService bankService;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("api/create-account")
-    public String createAccount(){
+
+    // @RequestyBody: recebe as informações do PostMan e armazena no parâmetro request.
+    public String createAccount(@RequestBody AccountRequest request){
+        System.out.println("entrou no método createAccount");
+
+        // passando o parâmetro para bankService
+        bankService.createAccount(request);
         return "sucess";
     }
 }
